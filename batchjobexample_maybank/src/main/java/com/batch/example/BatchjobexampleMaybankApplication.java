1e67508440398b92f2b60275ab1c7f6823c9f6e1
Tausif <tausif.anwar.92@gmail.com>
2025-03-06 01:00:25 +0800

    Springbatch assignment

---
+++ batchjobexample_maybank/src/main/java/com/batch/example/BatchjobexampleMaybankApplication.java
@@ -0,0 +1,13 @@
+package com.batch.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchjobexampleMaybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchjobexampleMaybankApplication.class, args);
+	}
+
+}
---
+++ batchjobexample_maybank/src/main/java/com/batch/example/config/CustomItemProcessor.java
@@ -0,0 +1,26 @@
+package com.batch.example.config;
+
+import com.batch.example.model.Product;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomItemProcessor implements ItemProcessor<Product, Product> {
+    @Override
+    public Product process(Product item) throws Exception {
+
+
+        try {
+            System.out.println(item.getDescription());
+            int discountPer = Integer.parseInt(item.getDiscount().trim());
+            double originalPrice = Double.parseDouble(item.getPrice().trim());
+            double discount = (discountPer / 100) * originalPrice;
+            double finalPrice = originalPrice - discount;
+            item.setDiscountedPrice(String.valueOf(finalPrice));
+        } catch (
+                NumberFormatException ex
+        ) {
+            ex.printStackTrace();
+        }
+
+        return item;
+    }
+}
